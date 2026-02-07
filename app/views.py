1d45bdef20b021afd6432271644b6ad94b0ccd9a
realkendo <kennethistifanus1@gmail.com>
2026-02-07 12:39:38 +0100

    templates

--- app/views.py
+++ app/views.py
@@ -1,35 +1,9 @@
 from app import app 
-from flask import url_for
+from flask import render_template
 
 # home route
 @app.route('/')
 def index():
-  return "index"
-
-# login route
-@app.route('/login')
-def login():
-  return "login"
-
-# user's route
-@app.route('/users/<username>')
-def profile(username):
-  return f'{username}\'s profile'
-
-with app.test_request_context():
-  print(url_for('index'))
-  print(url_for('login'))
-  print(url_for('login', next='/'))
-  print(url_for('profile', username="John Doe"))
-
-# error handling
-@app.errorhandler(404)
-def page_not_found(error):
-  return "This page does not exist.", 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-  return "Internal server error.", 500
-
+  return render_template('index.html', title='Home Page', name='Ken Dough', messages=["Hello", "Hi", "Hey"])
 
 
