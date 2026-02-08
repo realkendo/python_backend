3fe0141a621decbb74310ee5bc66decbdd5194b1
realkendo <kennethistifanus1@gmail.com>
2026-02-08 23:09:24 +0100

    contact forms

--- app/views.py
+++ app/views.py
@@ -1,9 +1,22 @@
 from app import app 
 from flask import render_template
+from flask import request, redirect, url_for
 
 # home route
 @app.route('/')
 def index():
   return render_template('index.html', title='Home Page', name='Ken Dough', messages=["Hello", "Hi", "Hey"])
 
+# contact form route
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
+
+# route to handle the form post request
+@app.route('/handle_form', methods=['POST'])
+def handle_form():
+  name = request.form['name']
+  email = request.form['email']
+  # process or store the form data here
+  return redirect(url_for("Thank you"))
 
