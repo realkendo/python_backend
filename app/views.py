ee6ebd3371d4eacd12bccaf3d88e18f2c2533650
realkendo <kennethistifanus1@gmail.com>
2026-02-09 01:17:43 +0100

    file upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app 
 from flask import render_template
 from flask import request, redirect, url_for
+# file import
+import os
+from werkzeug.utils import secure_filename
 
 # home route
 @app.route('/')
@@ -20,3 +23,11 @@ def handle_form():
   # process or store the form data here
   return redirect(url_for("Thank you"))
 
+# file upload route
+@app.route('/handle_file_upload', methods=['POST'])
+def handle_file_upload():
+  file = request.file['file']
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  return "File upload successful"
