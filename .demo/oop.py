a15192c1c97058359567da9ff8f9aac25261f2f5
realkendo <kennethistifanus1@gmail.com>
2026-02-15 17:40:07 +0100

    updates

---
+++ .demo/oop.py
@@ -0,0 +1,12 @@
+class Dog:
+  pass
+
+german_shephard = Dog()
+chihuahua = Dog()
+
+german_shephard.age = 5
+chihuahua.age = 2
+
+print(german_shephard.age)
+print(chihuahua)
+
--- app/models.py
+++ app/models.py
@@ -56,3 +56,14 @@ user = User.query.options(joinedload(User.posts)).filter_by(username='johndoe').
 for post in user.posts:
   print(post.title)
 
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+  username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+  password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+  submit = SubmitField('Login')
+  
