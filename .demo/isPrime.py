608718832bcd49e789951e7048ac5dd53219d88d
realkendo <kennethistifanus1@gmail.com>
2026-03-11 23:46:24 +0100

    test

---
+++ .demo/isPrime.py
@@ -0,0 +1,13 @@
+input_number = input("Please type the number to check: ")
+
+num = int(input_number)
+
+# if num < 0:
+#   print(f'{num} cannot be a negative integer')
+# elif num == 1 or num == 2 or num == 3:
+#   print(f'{num} is a prime number')
+# elif num > 3 and num % 
+#   print(f'{num} is an even number')
+# else:
+#   print(f'{num} is an odd number')
+
--- .demo/oop.py
+++ .demo/oop.py
@@ -1,12 +1,29 @@
-class Dog:
+import math 
+class Point:
+
+  def move(self, x, y):
+    self.x = x
+    self.y = y
+
+  def reset(self):
+    self.move(0, 0)
+
+  def calculate_distance(self, other_point):
+    return math.sqrt(
+      (self.x - other_point.x) **2 + 
+      (self.y - other_point.y)**2)
   pass
 
-german_shephard = Dog()
-chihuahua = Dog()
 
-german_shephard.age = 5
-chihuahua.age = 2
 
-print(german_shephard.age)
-print(chihuahua)
+p1 = Point()
+p2 = Point()
 
+p1.reset()
+p2.move(5,0)
+
+print(p2.calculate_distance(p1))
+assert (p2.calculate_distance(p1) == p1.calculate_distance(p2))
+p1.move(3,4)
+print(p1.calculate_distance(p2))
+print(p1.calculate_distance(p1))
